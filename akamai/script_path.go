95ee5746c2ad3ee0c8b02c572e322b7bec3af90e
fabianissam <44288705+fabianissam@users.noreply.github.com>
2026-01-23 23:52:25 +0100

    Implement ParseSbsdScriptPath for Sbsd SDK path extraction
    Add ParseSbsdScriptPath function to extract SDK path.

--- akamai/script_path.go
+++ akamai/script_path.go
@@ -8,7 +8,7 @@ import (
 
 var (
 	scriptPathExpr = regexp.MustCompile(`<script type="text/javascript"\s+(?:nonce=".*")?\s+src="((?i)[a-z\d/\-_]+)"></script>`)
-
+	sbsdScriptPathExpr = regexp.MustCompile(`<script type="text/javascript"\s+(?:nonce="[^"]*")?\s+src="((?i)[a-z\d/\-_]+)\?v=([^"'&]+)"`)
 	ErrScriptPathNotFound = errors.New("hyper-sdk: script path not found")
 )
 
@@ -25,4 +25,18 @@ func ParseScriptPath(reader io.Reader) (string, error) {
 	}
 
 	return string(matches[1]), nil
+}
+
+// ParseSbsdScriptPath get the Akamai Bot Manager web Sbsd SDK path from the given HTML code src.
+// path, v-param, error
+func ParseSbsdScriptPath(reader io.Reader) (string,string,error) {
+	src, err := io.ReadAll(reader)
+	if err != nil {
+		return "", "", errors.Join(ErrScriptPathNotFound, err)
+	}
+	matches := regex.FindSubmatch(body)
+		if len(matches) < 3 {
+			return "", "", ErrScriptPathNotFound
+		}
+	return string(matches[1]), string(matches[2]), nil
 }
