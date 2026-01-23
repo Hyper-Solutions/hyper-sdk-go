a1d131c5d2b74d33a4c8aa0e659ddcae9b3ed95a
fabianissam <44288705+fabianissam@users.noreply.github.com>
2026-01-23 23:54:37 +0100

    Add tests and benchmark for ParseSbsdScriptPath

--- akamai/script_path_test.go
+++ akamai/script_path_test.go
@@ -29,3 +29,28 @@ func BenchmarkParseScriptPath(b *testing.B) {
 		script.Seek(0, io.SeekStart)
 	}
 }
+
+func TestParseSbsdScriptPath(t *testing.T) {
+	path, err := ParseSbsdScriptPath(strings.NewReader(`<script type="text/javascript"  src="/QaZkxg1hkkBeBuc7J_KFVltz/tEz56cktSmc7/XEcFAQ/Aj4OUi/EqXkc?v=83cfdce1-9f29-c905-41c3-cf81f625170f"></script>`))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(path)
+}
+
+func BenchmarkParseSbsdScriptPath(b *testing.B) {
+	script := strings.NewReader(`<script type="text/javascript"  src="/QaZkxg1hkkBeBuc7J_KFVltz/tEz56cktSmc7/XEcFAQ/Aj4OUi/EqXkc?v=83cfdce1-9f29-c905-41c3-cf81f625170f"></script>`)
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		_, err := ParseSbsdScriptPath(script)
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		script.Seek(0, io.SeekStart)
+	}
+}
+
